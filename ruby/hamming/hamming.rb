bd30d9a53881477d9dd35ada1f9f074cc191d082
neimad971 <damientalbot26@gmail.com>
2019-04-08 19:49:40 +0200

    provides a more modular and refined hamming class

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,13 +1,26 @@
-class Hamming
-  def self.compute(strand1, strand2)
-    unless strand1.size == strand2.size
-      raise ArgumentError.new("The DNA strands don't have the same lenght")
-    end
-    
-    # strand1.chars.zip(strand2.chars).reject { |pair| pair[0] == pair[1] }.size
-    # strand1.chars.each_index.reject { |nucleic| strand1[nucleic] == strand2[nucleic] }.size
-    # strand1.chars.zip(strand2.chars).count { |pair| pair[0] != pair[1] }
-    strand1.chars.each_index.count { |nucleic| strand1[nucleic] != strand2[nucleic] }
+module ConvenienceMethodsForHamming
+  def compute(strand1, strand2)
+    new(strand1, strand2).distance
+  end
+end
 
+class StrandLengthError < ArgumentError
+  def initialize(message = 'Strand Lengths must be identical!')
+    super
+  end
+end
+
+class Hamming
+  extend ConvenienceMethodsForHamming
+  attr_reader :nucleotides
+
+  def initialize(strand1, strand2)
+    raise StrandLengthError unless strand1.size == strand2.size
+
+    @nucleotides = strand1.chars.zip(strand2.chars)
+  end
+
+  def distance
+    nucleotides.count { |n1, n2| n1 != n2 }
   end
 end
