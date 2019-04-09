c3a1e4cbf982ffc745ee20b165db35a2cadaadbb
neimad971 <damientalbot26@gmail.com>
2019-04-09 20:47:33 +0200

    changes design from bunch of ifs to hash

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,28 +1,11 @@
 class Raindrops
   def self.convert(number)
     sound = ""
-    if number % 3 == 0
-      sound << 'Pling'
+    factors = { 3 => 'Pling', 5 => 'Plang', 7 => 'Plong' }
+    sound = "#{number}" if factors.each_key.all? { |k| (number % k) != 0 }
+    factors.each do |k, v|
+      sound << v if number % k == 0
     end
-
-    if number % 5 == 0
-      sound << 'Plang'
-    end
-
-    if number % 7 == 0
-      sound << 'Plong'
-    end
-
-    if number % 3 != 0 && number % 5 != 0 && number % 7 != 0
-      sound = "#{number}"
-    end
-
     sound
   end
 end
-
-
-# if the number has 3 as a factor, output 'Pling'.
-# If the number has 5 as a factor, output 'Plang'.
-# If the number has 7 as a factor, output 'Plong'.
-# If the number does not have 3, 5, or 7 as a factor, just pass the number's digits straight through.
