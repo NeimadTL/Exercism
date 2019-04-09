2233fd315d995b410e4a8bd0be53fd5e789439e6
neimad971 <damientalbot26@gmail.com>
2019-04-09 21:37:42 +0200

    cleans things up and makes the code more readable

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,11 +1,19 @@
 class Raindrops
+
+  FACTORS_TABLE = {
+    3 => 'Pling',
+    5 => 'Plang',
+    7 => 'Plong'
+  }
+
   def self.convert(number)
-    sound = ""
-    factors = { 3 => 'Pling', 5 => 'Plang', 7 => 'Plong' }
-    sound = "#{number}" if factors.each_key.all? { |k| (number % k) != 0 }
-    factors.each do |k, v|
-      sound << v if number % k == 0
-    end
+    indivisible?(number) ? sound = "#{number}" : sound = ''
+    FACTORS_TABLE.each { |k, v| sound << v if number % k == 0 }
     sound
   end
+
+  def self.indivisible?(number)
+    FACTORS_TABLE.each_key.all? { |k| (number % k) != 0 }
+  end
+
 end
