6df72aadc407679659b51a807b86b1e020e28871
neimadtl <damientalbot26@gmail.com>
2019-04-10 16:32:40 +0200

    adds different versions of the convert methods

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -6,11 +6,29 @@ class Raindrops
     7 => 'Plong'
   }
 
-  def self.convert(number)
-    indivisible?(number) ? sound = "#{number}" : sound = ''
-    FACTORS_TABLE.each { |k, v| sound << v if number % k == 0 }
-    sound
+  def self.convert1(number)
+    return "#{number}" if indivisible?(number)
+    FACTORS_TABLE.each_with_object('') do |key_value, sound|
+      sound << key_value[1] if number % key_value[0] == 0 
+    end
   end
+
+  def self.convert2(number)
+    sound = ''
+    FACTORS_TABLE.each_key do |key| 
+      sound << FACTORS_TABLE[key] if number % key == 0 
+    end
+    sound.empty? ? number.to_s : sound
+  end
+
+  def self.convert(number)
+    return "#{number}" if indivisible?(number)
+    FACTORS_TABLE.each_with_object('') do |pair, sound|
+      sound << pair[1] if number % pair[0] == 0
+    end
+  end
+
+
 
   def self.indivisible?(number)
     FACTORS_TABLE.each_key.all? { |k| (number % k) != 0 }
