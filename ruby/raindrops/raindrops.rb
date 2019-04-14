569d5d999a46ba31c47ae16eb11823f80c4a7a0a
neimad971 <damientalbot26@gmail.com>
2019-04-14 16:04:34 +0200

    Cleans up the code
    Using better name and removing different version of the convert method

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -6,47 +6,17 @@ end
 
 
 class Raindrops
-
-  FACTORS_TABLE = {
+  
+  RULES = {
     3 => 'Pling',
     5 => 'Plang',
     7 => 'Plong'
   }
 
-  def self.convert1(number)
-    indivisible?(number) ? sound = number.to_s : sound = ''
-    FACTORS_TABLE.each { |k, v| sound << v if number % k == 0 }
-    sound
-  end
-
-  def self.convert2(number)
-    sound = ''
-    FACTORS_TABLE.each_key do |key| 
-      sound << FACTORS_TABLE[key] if number % key == 0 
-    end
-    sound.empty? ? number.to_s : sound
-  end
-
-  def self.convert3(number)
-    return number.to_s if indivisible?(number)
-    FACTORS_TABLE.each_with_object('') do |pair, sound|
-      sound << pair[1] if number % pair[0] == 0
-    end
-  end
-
   def self.convert(number)
-    sound = FACTORS_TABLE.each_with_object('') do |(factor, note), sound|
+    sound = RULES.each_with_object('') do |(factor, note), sound|
       sound << note if number.factor_of?(factor)
     end
     sound.empty? ? number.to_s : sound
   end
-
-
-
-
-
-  def self.indivisible?(number)
-    FACTORS_TABLE.each_key.all? { |k| (number % k) != 0 }
-  end
-
 end
