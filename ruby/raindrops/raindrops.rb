a740db3f4e078cf199f3b0244b8a26faa5b25fd5
neimad971 <damientalbot26@gmail.com>
2019-04-16 20:56:34 +0200

    Makes a Raindrops class

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -6,17 +6,35 @@ end
 
 
 class Raindrops
-  
+
   RULES = {
     3 => 'Pling',
     5 => 'Plang',
     7 => 'Plong'
   }
+  attr_reader :intensity
 
-  def self.convert(number)
-    sound = RULES.each_with_object('') do |(factor, note), sound|
-      sound << note if number.factor_of?(factor)
-    end
-    sound.empty? ? number.to_s : sound
+  def initialize(intensity)
+    @intensity = intensity
   end
+
+  def fall
+    make_noise(sound).to_s
+  end
+
+  def self.convert(intensity)
+    Raindrops.new(intensity).fall
+  end
+
+  private
+  
+    def sound
+      RULES.each_with_object('') do |(factor, note), sound|
+        sound << note if intensity.factor_of?(factor)
+      end
+    end
+
+    def make_noise(sound)
+      sound.empty? ? intensity : sound
+    end
 end
